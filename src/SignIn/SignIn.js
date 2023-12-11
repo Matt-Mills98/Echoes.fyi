1301f1ee24840aba788181c0e1fb0e2d921ba0e9
Matt <mattmills32013@gmail.com>
2023-12-10 22:02:23 -0500

    Sign In Margin

--- src/SignIn/SignIn.js
+++ src/SignIn/SignIn.js
@@ -196,7 +196,7 @@ export default function SignIn(props) {
                                 }}>
                                     <Fade in={page == ''} >
                                         <Card sx={{
-                                            bgcolor: '#16191a', height: '50vh', margin: '20px', alignItems: 'center', display: "flex", flexDirection: "column",
+                                            bgcolor: '#16191a', height: '50vh', ml: '20px',mt: '20px',mb: '20px',mr: '20px', alignItems: 'center', display: "flex", flexDirection: "column",
                                             justifyContent: "center"
                                         }}>
 
